6aefac38ad0f11dd4ad7321563db035442b4db5d
majoalba20 <mjalba458@gmail.com>
2022-05-20 22:59:24 -0500

    fixing model and controllers v4

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,25 +4,27 @@ import { createAdmin, deleteAdmin, getAdmin, getOneAdmin, updateAdmin } from "..
 import { createBusiness, deleteBusiness, getBusiness, getOneBusiness, updateBusiness } from "../controllers/BusinessController.js";
 import { createInterests, deleteInterests, getInterests, getOneInterests, updateInterests } from "../controllers/InterestsController.js";
 
-import { createMentor, deleteMentor, getMentor, getOneMentor, updateMentor, getMentorsAvailable, getAllMentors } from "../controllers/MentorController.js";
+import { createMentor, deleteMentor, getMentor, getOneMentor, updateMentor, getAllMentors } from "../controllers/MentorController.js";
+
 import { createPrograms, deletePrograms, getOnePrograms, getPrograms, updatePrograms } from "../controllers/ProgramsController.js";
 import { createSessions, deleteSessions, getOneSessions, getSessions, updateSessions, getAllSessions } from "../controllers/SessionsController.js";
 import { createStudent, deleteStudent, getOneStudent, getStudent, updateStudent, getAllStudents, searchStudent, getMaxCohort, getStudentsAvailable } from "../controllers/StudentController.js";
 import { createStudies, deleteStudies, getOneStudies, getStudies, updateStudies } from "../controllers/StudiesController.js";
 import { createUsers, deleteUsers, getOneUsers, getUsers, updateUsers } from "../controllers/UsersController.js";
 import { checkLogin } from "../controllers/LoginController.js";
-import { getMatch, getMatchCohort, updateMatch, calculateMatch, updateMatchAutomatic, createMatch } from "../controllers/MatchController.js";
+import { createUserStudent, deleteAllUserStudent, getUserStudent } from "../controllers/userStudentsController.js";
+// import { getMatch, getMatchCohort, updateMatch, calculateMatch, updateMatchAutomatic, createMatch } from "../controllers/MatchController.js";
 
 const router = express.Router();
 
 router.get('/login/:email/:password', checkLogin)
 
-router.get('/match/calculate/:id_student', calculateMatch)
-router.get('/match/:cohort/:program', getMatchCohort)
-router.get('/matchs', getMatch)
-router.put('/match/update/:id_student/:id_mentor', updateMatch)
-router.put('/match/confirm', updateMatchAutomatic)
-router.post('/match/create', createMatch)
+// router.get('/match/calculate/:id_student', calculateMatch)
+// router.get('/match/:cohort/:program', getMatchCohort)
+// router.get('/matchs', getMatch)
+// router.put('/match/update/:id_student/:id_mentor', updateMatch)
+// router.put('/match/confirm', updateMatchAutomatic)
+// router.post('/match/create', createMatch)
 
 router.get('/admin', getAdmin) //funciona
 router.get('/admin/:id', getOneAdmin) //funciona
@@ -30,15 +32,14 @@ router.post('/admin', createAdmin)  // funciona
 router.put('/admin/:id', updateAdmin) //funciona
 router.delete('/admin/:id', deleteAdmin) //funciona
 
-//revisar
+//revisar 
 router.get('/all-mentors', getAllMentors) //funciona
-router.get('/mentors', getMentor)
-router.get('/mentor/:id', getOneMentor) //
-router.post('/mentor', createMentor)
-router.put('/mentor/:id', updateMentor)
-router.delete('/mentor/:id', deleteMentor)
-router.delete('/admin/:id', deleteMentor)
-router.get('/mentors/available', getMentorsAvailable)
+router.get('/mentors', getMentor) //funciona
+router.get('/mentor/:id', getOneMentor) //funciona
+router.post('/mentor', createMentor) //funciona
+router.put('/mentor/:id', updateMentor) //funciona
+router.delete('/mentor/:id', deleteMentor) //funciona
+// router.get('/mentors/available', getMentorsAvailable)
 
 router.get('/all-sessions', getAllSessions) //funciona
 router.get('/sessions', getSessions) //funciona
@@ -94,5 +95,8 @@ router.post('/users', createUsers) //funciona
 router.put('/users/:id', updateUsers) //funciona
 router.delete('/users/:id', deleteUsers) //funciona
 
+router.get('/user/student', getUserStudent) //funciona
+router.post('/create/userStudent', createUserStudent) //funciona
+router.delete('/delete/userStudent', deleteAllUserStudent) //funciona
 
 export default router
