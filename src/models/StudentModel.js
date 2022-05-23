8595898db0f23eda62d27277d878f35fa178ee73
Trueqo <julianochoa1999@gmail.com>
2022-05-23 10:47:15 -0500

    cambios aceptados

--- src/models/StudentModel.js
+++ src/models/StudentModel.js
@@ -7,17 +7,9 @@ const StudentModel = db.define('Estudiantes', {
     email: { type: DataTypes.STRING },
     cohort: { type: DataTypes.INTEGER },
     age: { type: DataTypes.INTEGER },
-<<<<<<< HEAD
     phone: { type: DataTypes.INTEGER },
     status: { type: DataTypes.BOOLEAN },
     gender: { type: DataTypes.STRING }
-=======
-    phone: { type: DataTypes.BIGINT },
-    status: { type: DataTypes.INTEGER },
-    gender: { type: DataTypes.STRING },
-    
-    
->>>>>>> feature/julian
 })
 
 export default StudentModel;
