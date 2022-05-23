51bf002fb4e406517c60598aca06fc9923e4951d
Trueqo <julianochoa1999@gmail.com>
2022-05-23 10:49:25 -0500

    acepto mis varibales de entorno de la db

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,8 @@
 import { Sequelize } from "sequelize";
 
-<<<<<<< HEAD
-const db = new Sequelize(process.env.DB_DATABASE || 'Okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
-=======
-
-
 
 
 const db = new Sequelize(process.env.DB_DATABASE || 'okhlos', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
->>>>>>> feature/julian
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
     port: 3307
