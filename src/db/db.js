2701a8d7a0275cd58b73f501d90144e2d0aadc86
Trueqo <julianochoa1999@gmail.com>
2022-05-23 23:40:36 -0500

    env bug

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 
 
-const db = new Sequelize(process.env.DB_DATABASE || 'heroku', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '123456',{
+const db = new Sequelize(process.env.DB_DATABASE || 'heroku', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
     host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
 
