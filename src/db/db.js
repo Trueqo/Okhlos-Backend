b965dde71eb7a00f9627436fab74410da12b5426
Trueqo <julianochoa1999@gmail.com>
2022-05-23 23:58:52 -0500

    .env solved

--- src/db/db.js
+++ src/db/db.js
@@ -2,8 +2,8 @@ import { Sequelize } from "sequelize";
 
 
 
-const db = new Sequelize(process.env.DB_DATABASE || 'heroku', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '',{
-    host: process.env.DB_HOST || 'localhost',
+const db = new Sequelize(process.env.DB_DATABASE || 'jqkuxc5h95dn7dkc', process.env.DB_USER || 'x3wcuaof7qkdm3xl', process.env.DB_PASSWORD || 'hdkwycrjmv9n43nh',{
+    host: process.env.DB_HOST || 'cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
 
 })
